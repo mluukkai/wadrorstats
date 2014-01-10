d2098d888af4270f7f056f542301657054329da5
Matti Luukkainen <mluukkai@iki.fi>
2014-01-10 19:16:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wadrorstats::Application.config.secret_key_base = '09b78ddf01e061ed536f3034137748b00d696147d70b91d1c2de654656e6a0376c24ba4a613cc1586a9fd77798b1b37b192749a37865bf10dc3333c2bcf1a98a'
