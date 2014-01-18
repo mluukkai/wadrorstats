04ec194eaa6260a4fdb16e478833b7e16d871589
Matti Luukkainen <mluukkai@iki.fi>
2014-01-19 00:51:17 +0200

    auth removed from api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,6 @@
 class ApiController < ApplicationController
+  before_filter :authenticate, :except => [:week_stats]
+
   def week_stats
     weeks = {}
     (1..Course.current.current_week).each do |w|
