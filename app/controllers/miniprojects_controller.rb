bae707451410de29fec83a462dfcd79505a62ad5
Matti Luukkainen <mluukkai@iki.fi>
2016-04-20 17:46:05 +0300

    debug...

--- app/controllers/miniprojects_controller.rb
+++ app/controllers/miniprojects_controller.rb
@@ -47,6 +47,8 @@ class MiniprojectsController < ApplicationController
   # PATCH/PUT /miniprojects/1
   # PATCH/PUT /miniprojects/1.json
   def update
+    puts "------------------------"
+    puts params[:id]
     @miniproject = Miniproject.find_by(identifier:params[:id])
     if @miniproject.update(miniproject_params)
       redirect_to miniproject_path(@miniproject.identifier), notice: 'Miniproject updated'
